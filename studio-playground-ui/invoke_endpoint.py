72494024dedc25c6a4250c376cf878f7ed6225ff
Dipika <dikhulla@amazon.com>
2023-03-21 05:42:32 +0000

    added json uploader and json editor through streamlit-ace

--- studio-playground-ui/invoke_endpoint.py
+++ studio-playground-ui/invoke_endpoint.py
@@ -1,11 +1,11 @@
 import streamlit as st
+from streamlit_ace import st_ace
 import boto3
 import json
 import io
 import jinja2
 import os
 from pathlib import Path
-from PIL import Image
 
 sagemaker_runtime = boto3.client('runtime.sagemaker')
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
@@ -42,6 +42,9 @@ def generate_text(payload, endpoint_name):
     )
     print("Model input: \n", encoded_input)
     result = json.loads(response['Body'].read().decode()) # -
+    
+    # TO DO: results are either dictionary or list
+    print(result) 
     for item in result:
         if isinstance(item, list):
             for element in item:
@@ -58,6 +61,84 @@ def handle_stable_diffusion(response):
     placeholder  = st.image(img_res)
     return prompt
 
+
+
+code = """
+{
+  "modelName": "Example-Model",
+  "endpoint_name": "huggingface-inference-endpoint",
+  "payload": {
+    "parameters": {
+      "length_penalty": 2.0,
+      "max_new_tokens": 20,
+      "early_stopping": false
+    }
+  }
+}
+"""
+
+
+def get_user_input():
+    uploaded_file = st.file_uploader(label="Upload JSON Template", type=["json"])
+    uploaded_file_location = st.empty()
+
+    # user uploads an image
+    if uploaded_file is not None:
+        input_str = json.load(uploaded_file)
+        if validate_json_template(input_str):
+            user_file_path = os.path.join("templates", 
+                                          input_str["modelName"] + ".template.json")
+            with open(user_file_path, "wb") as user_file:
+                user_file.write(uploaded_file.getbuffer())
+            uploaded_file_location.write("Template Uploaded: " + str(user_file_path))
+        else:
+            uploaded_file_location.warning("Invalid Input: please upload a valid template.json")
+    else:
+        user_file_path = None
+        
+    return user_file_path
+
+
+@st.cache_data
+def validate_json_template(json_dictionary):
+    expected_keys = {"modelName", "endpoint_name", "payload"}
+    actual_keys = set(json_dictionary.keys())
+
+    if not expected_keys.issubset(actual_keys):
+        st.warning(
+            "Invalid Input: template.json must contain a modelName, endpoint_name, and payload keys"
+        )
+        return False
+
+    if not "parameters" in json_dictionary["payload"].keys():
+        st.warning(
+            "Invalid Input: template.json must contain a payload key with parameters listed"
+        )
+        return False
+    return True
+
+
+@st.cache_data
+def handle_editor_content(input_str):
+    if validate_json_template(input_str):
+            try:
+                model_name = input_str["modelName"]
+                filename = model_name + ".template.json"
+                user_file_path = os.path.join("templates", filename)
+                f = codecs.open(user_file_path, "w", encoding="utf-8")
+                json_out = json.dumps(input_str, sort_keys=False, indent=4)
+                f.write(json_out)
+                f.flush()
+                o = open(user_file_path)
+                ojson = o.read()
+                st.write("json saved at " + str(user_file_path))
+            except Exception as e:
+                st.write(e)
+                
+
+
+
+
 count, fileList, fileNames = read_template_dir('templates')
 
 
@@ -73,11 +154,39 @@ parameters = output['payload']['parameters']
 
 ########## UI Code #############
 st.sidebar.title("Model Parameters")
+st.image('./ml_image_prompt.png')
 
-image = Image.open('./ml_image_prompt.png')
-new_image = image.resize((900, 200))
-st.image(new_image)
-# st.header("Prompt Engineering Playground")
+
+# Adding your own model
+st.header("Add a New Model")
+st.write(
+        """Add a new model by uploading a template.json file or by pasting the dictionary in
+        the editor. A model template
+        is a json dictionary containing a modelName, endpoint_name, and payload with
+        parameters.  \n \n Below is an
+        example of a template.json"""
+    )
+st.code(code, language="json")
+get_user_input()
+    
+# Spawn a new Ace editor and isplay editor's content as you type
+content = st_ace(
+    theme="tomorrow_night",
+    wrap=True,
+    show_gutter=True,
+    language="json",
+    placeholder= code,
+    keybinding = "vscode",
+    min_lines = 15
+)
+
+if content:
+    input_str = json.loads(content)
+    handle_editor_content(input_str)
+
+
+
+st.header("Prompt Engineering Playground")
 for x in parameters: 
     if isinstance(parameters[x], bool):
         parameters[x] = st.sidebar.selectbox(x,['True', 'False' ] )
@@ -96,15 +205,15 @@ for x in parameters:
 
 st.markdown("""
 
-Example for :orange[ Few Shot Learning]
+Example :red[For Few Shot Learning]
 
 **:blue[List the Country of origin of food.]**
-- Pizza comes from Italy
-- Burger comes from USA
-- Curry comes from
+Pizza comes from Italy
+Burger comes from USA
+Curry comes from
 """)
 
-prompt = st.text_area("Enter your prompt here:", height=400)
+prompt = st.text_area("Enter your prompt here:", height=350)
 placeholder = st.empty()
 
 if st.button("Run"):
