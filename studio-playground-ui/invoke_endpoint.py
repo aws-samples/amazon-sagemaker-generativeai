4f90cf38fdce65f829772a1bb198e9149635ba7c
Jose Navarro <58894258+joseanavarrom@users.noreply.github.com>
2023-09-28 00:43:11 +0200

    reverting back to previous commit - this is the final commit of pull request

--- studio-playground-ui/invoke_endpoint.py
+++ studio-playground-ui/invoke_endpoint.py
@@ -33,7 +33,7 @@ def generate_text_ai21(payload, endpoint_name):
     print("payload type: ", type(payload))
     print("payload: ", payload)
     encoded_input = json.dumps({
-        "prompt": payload["text_inputs"],
+        "prompt": payload["inputs"],
         "maxTokens": payload["maxTokens"],
         "temperature": payload["temperature"],
         "numResults": payload["numResults"]}).encode("utf-8")
@@ -48,7 +48,7 @@ def generate_text_ai21(payload, endpoint_name):
 
 def generate_text_ai21_summarize(payload, endpoint_name):
     encoded_input = json.dumps({
-        "source": payload["text_inputs"],
+        "source": payload["inputs"],
         "sourceType": "TEXT"}).encode("utf-8")
     response = sagemaker_runtime.invoke_endpoint(
         EndpointName=endpoint_name, ContentType="application/json", Body=encoded_input
@@ -60,10 +60,10 @@ def generate_text_ai21_summarize(payload, endpoint_name):
 
 
 def generate_text_ai21_context_qa(payload, question, endpoint_name):
-    print('----- Context -------', payload["text_inputs"])
+    print('----- Context -------', payload["inputs"])
     print('----- Question ------', question)
     encoded_input = json.dumps({
-        "context": payload["text_inputs"],
+        "context": payload["inputs"],
         "question": question}).encode("utf-8")
     response = sagemaker_runtime.invoke_endpoint(
         EndpointName=endpoint_name, ContentType="application/json", Body=encoded_input
