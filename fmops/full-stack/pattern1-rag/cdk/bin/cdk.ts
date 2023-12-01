c0782dc91983e76773c3c4768b108fdb5b8f8b34
Abdullahi Olaoye <olaoyea@amazon.com>
2023-12-01 16:10:18 -0600

    update cdk code

--- fmops/full-stack/pattern1-rag/cdk/bin/cdk.ts
+++ fmops/full-stack/pattern1-rag/cdk/bin/cdk.ts
@@ -6,13 +6,20 @@ import { FrontendStack } from '../lib/frontend-stack';
 
 
 const app = new cdk.App();
-const backendStack = new BackendStack(app, 'BackendStack', { env: { region: 'us-east-1' } });
-const frontendStack = new FrontendStack(app, 'FrontendStack', {
-  vpc: backendStack.backendStackProps.vpc,
-  openSearchDomain: backendStack.backendStackProps.openSearchDomain,
-  openSearchDomainArn: backendStack.backendStackProps.openSearchDomainArn,
-  firehoseName: backendStack.backendStackProps.firehoseName,
-  firehoseArn: backendStack.backendStackProps.firehoseArn,
-  bucket: backendStack.backendStackProps.bucket,
-  env: { region: 'us-east-1' }
-});
+const bundlingStacks = app.node.tryGetContext("aws:cdk:bundling-stacks") as Array<string>;
+const buildAllStacks = bundlingStacks.includes("**");
+
+if (buildAllStacks) {
+    const backendStack = new BackendStack(app, "BackendStack", { env: { region: 'us-east-1' } });
+    const frontendStack = new FrontendStack(app, "FrontendStack", {
+      vpc: backendStack.backendStackProps.vpc,
+      openSearchDomain: backendStack.backendStackProps.openSearchDomain,
+      openSearchDomainArn: backendStack.backendStackProps.openSearchDomainArn,
+      firehoseName: backendStack.backendStackProps.firehoseName,
+      firehoseArn: backendStack.backendStackProps.firehoseArn,
+      bucket: backendStack.backendStackProps.bucket,
+      env: { region: 'us-east-1' }
+    });
+} else {
+  new BackendStack(app, "BackendStack", { env: { region: 'us-east-1' } });
+}
