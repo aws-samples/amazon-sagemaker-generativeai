8a58ebe1457a04c460e40311e3fee93fa4769489
SuryaKari <karisury@amazon.com>
2025-11-10 20:09:51 +0000

    Updated SAMA Rl

--- 8_rl_cookbook/sama_rl/__init__.py
+++ 8_rl_cookbook/sama_rl/__init__.py
@@ -4,6 +4,8 @@ SAMA RL - Simple API for Reinforcement Learning with Language Models
 
 # Main algorithm classes
 from .grpo import GRPO
+from .ppo import PPO
+from .grpo_rlvr import GRPO_RLVR
 
 # Configuration utilities
 from .config_loader import load_config
@@ -17,6 +19,8 @@ from .inference import EndpointInference, create_inference_model
 __version__ = "0.1.0"
 __all__ = [
     "GRPO",
+    "PPO",
+    "GRPO_RLVR",
     "ModelDeployer",
     "deploy_grpo_model",
     "EndpointInference",
